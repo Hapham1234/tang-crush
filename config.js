c288f5b8a642564f52c91dcb64b6c564f328da8b
Hapham1234 <86770189+Hapham1234@users.noreply.github.com>
2021-07-01 15:20:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Suna à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao anh cứ đi nhầm đường
+    Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnIntro: 'hehe',
+    title: 'Em lỡ yêu anh rồi 😳',
+    desc: 'A-anh yêu em khum zậy? ',
+    btnYes: 'Tất nhiên rồi công chúa của anh',
+    btnNo: 'Khum em :3',
+    question:'Sao anh lại yêu em vậy :33?',
+    btnReply: 'Gửi đến trái tim Hà <3',
+    reply: 'Ai biết được :"Đ ',
+    mess: ':"))))',
+    messDesc: 'Thôi kệ đi, em vẫn yêu Suna.',
+    btnAccept: 'Hehe :33',
+    messLink: 'https://m.me/binghe.luo.585'
 }
